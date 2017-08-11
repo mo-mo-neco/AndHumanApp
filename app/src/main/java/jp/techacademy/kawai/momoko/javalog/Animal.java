08507561bf5271c29165be81838ddd4e5f376c49
mo-mo-neco <momomogarden@gmail.com>
2017-08-11 19:30:41 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kawai/momoko/javalog/Animal.java
@@ -0,0 +1,13 @@
+package jp.techacademy.kawai.momoko.javalog;
+
+/**
+ * Created by momon on 2017/08/11.
+ */
+
+abstract class Animal {
+    // メンバ変数
+    String name;  // 名前
+    int age;    // 年齢
+
+    abstract public void say();
+}
---
+++ app/src/main/java/jp/techacademy/kawai/momoko/javalog/Thinkable.java
@@ -0,0 +1,9 @@
+package jp.techacademy.kawai.momoko.javalog;
+
+/**
+ * Created by momon on 2017/08/11.
+ */
+
+interface Thinkable {
+    public void think();
+}
